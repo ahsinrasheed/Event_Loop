eb120e192cba5c5aa8eb9f7ba565920207129ce7
ahsinrasheed <ahsinbhatti5@gmail.com>
2022-06-08 00:32:46 +0500

    adding testModule-2 and performming exports functionality

--- modules.js
+++ modules.js
@@ -1,6 +1,22 @@
 // console.log(arguments);
 // console.log(require('module').wrapper);
 
+
+// module.export
+/*
 const C = require('./test-module1');
 const calc1 = new C();
 console.log(calc1.add(2, 5));
+*/
+
+// exports
+const calc2 = require('./test.module-2');
+console.log(calc2.multiply(2, 5));
+
+
+
+
+
+
+
+
