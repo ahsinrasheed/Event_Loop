10272753fce90c4e842440be443b70572072145c
ahsinrasheed <ahsinbhatti5@gmail.com>
2022-06-07 22:13:46 +0500

    add file events.js

---
+++ events.js
@@ -0,0 +1,19 @@
+const EventEmitter = require('events');
+
+
+const myEmitter = new EventEmitter();
+
+myEmitter.on('newSale', () => {
+    console.log('There was a new sale!');
+})
+
+myEmitter.on('newSale', () => {
+    console.log('Costumer Name : Ahsin');
+})
+
+myEmitter.on('newSale', stock => {
+    console.log(`There are now ${stock} items left in stock`); 
+})
+
+myEmitter.emit('newSale', 9);
+
